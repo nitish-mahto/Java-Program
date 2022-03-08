27b51718e6a9388e0a4c3d9cd2d5b1811500247a
Nitish Mahto <88816528+nitish-mahto@users.noreply.github.com>
2022-03-08 18:51:44 +0530

    Update prg20_4.java

--- prg20_4.java
+++ prg20_4.java
@@ -8,9 +8,26 @@ public class prg20_4 {
         System.out.print("Enter no :");
         int n = sc.nextInt();
 
+        /*
+         * for (int i = 1; i <= n; i++) {
+         * for (int j = 1; j <= i; j++) {
+         * System.out.print(j + "*".repeat(j));
+         * }
+         * System.out.println();
+         * }
+         */
+
+        /*
+         * In Case if repeat method (function) not work then we have to use below
+         * loop...
+         */
+
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j+"* ");
+                System.out.print(j);
+                for (int k = 1; k <= j; k++) {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
